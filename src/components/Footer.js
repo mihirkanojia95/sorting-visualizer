a2eebb700baacf8c20d8be8b8a3aa9add18dc5df
mihir kanojia <mihirkanojia95@gmail.com>
2024-06-26 16:36:28 +0530

    final commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="footer-warraper">
+            <div className="container">
+                <p className="copyright-text">Copyright &copy; 2024</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
